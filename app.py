11e02e38d2b3cba00c473e7b291505478163e1da
Andrin Sutter <andrin.sutter@ost.ch>
2025-01-28 22:48:18 +0100

    Anpassungen

--- app.py
+++ app.py
@@ -2,5 +2,20 @@ import streamlit as st
 
 st.title("Web App für Technik")
 
-st.sidebar.title("Navigation")
-st.sidebar.radio("Go to", ["Home"])
+start_page = st.Page("home.py", title="Home", icon=":material/home:", default=True)
+
+pg = st.navigation(
+        {
+            "Home": [start_page], #about_page, projektarbeit_page, logout_page
+            #"Die Formel 1": [formel1_page, drivers_page],
+            #"Analyse Q3 Silverstone 2024": [gp_page,
+            #                                track_page,
+            #                                speed_page,
+            #                                krümmungsradius_page,
+            #                                az_fz_page,
+            #                                haftreibung_page,
+            #                                faero_page],
+        }
+    )
+
+pg.run()
