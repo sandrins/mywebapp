2c38293e37b92566faee3bc4b49b4ccd05f0b1e9
Andrin Sutter <andrin.sutter@ost.ch>
2025-01-28 22:54:48 +0100

    Anpassungen

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
-st.title("Web App für Technik")
+#st.title("Web App für Technik")
 
 start_page = st.Page("pages/home.py", title="Home", icon=":material/home:", default=True)
+about_page = st.Page("pages/about.py", title="About", icon=":material/info:")
 
 pg = st.navigation(
     {
-        "Home": [start_page], #about_page, projektarbeit_page, logout_page
+        "Home": [start_page, about_page], #projektarbeit_page, logout_page
         #"Die Formel 1": [formel1_page, drivers_page],
         #"Analyse Q3 Silverstone 2024": [gp_page,
         #                                track_page,
