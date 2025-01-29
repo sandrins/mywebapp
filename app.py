6e0827b55f7f4b5a6e4adc0b04495a8b803f3f3b
Andrin Sutter <andrin.sutter@ost.ch>
2025-01-29 12:28:56 +0100

    Neue Seite

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
-#st.title("Web App für Technik")
-
 start_page = st.Page("pages/home.py", title="Home", icon=":material/home:", default=True)
 about_page = st.Page("pages/about.py", title="About", icon=":material/info:")
+projektarbeit_page = st.Page("pages/f1_start.py", title="Projektbeschreibung", icon=":material/sports_motorsports:")
 
 pg = st.navigation(
     {
         "Home": [start_page, about_page], #projektarbeit_page, logout_page
-        "Projekt Formel 1": [],
+        "Projekt Formel 1": [projektarbeit_page],
     }
 )
 
---
+++ pages/f1_start.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("Projektarbeit Formel 1")
